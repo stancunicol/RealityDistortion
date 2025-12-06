6871200abd6b6e4bbeba9b4922f609f1eccd45f8
vasilemarina <marinavasile826@gmail.com>
2025-12-07 00:37:25 +0200

    Added anomaly: creepy hand comes out of the TV when turned on.

--- RealityDistortion/Assets/Scripts/TV.cs
+++ RealityDistortion/Assets/Scripts/TV.cs
@@ -5,20 +5,29 @@ public class TVSwitch : MonoBehaviour
 {
     [SerializeField] private Material screenOff;
     [SerializeField] private Material screenVideo;
+    [SerializeField] private GameObject arm;
+    [SerializeField] private AnimationClip reachClip;
 
+    private Animator armAnimator;
     private VideoPlayer videoPlayer;
     private MeshRenderer meshRenderer;
     private bool isOn = false;
 
     void Start()
     {
+        armAnimator = arm != null ? arm.GetComponent<Animator>() : null;
         videoPlayer = GetComponent<VideoPlayer>();
         meshRenderer = GetComponent<MeshRenderer>();
+
         Material[] mats = meshRenderer.materials;
         mats[2] = screenOff;
         meshRenderer.materials = mats;
+
         if (videoPlayer != null)
             videoPlayer.Stop();
+
+        if (arm != null)
+            arm.SetActive(false);
     }
 
     void Update()
@@ -39,15 +48,33 @@ public class TVSwitch : MonoBehaviour
     void ToggleTV()
     {
         isOn = !isOn;
+
         Material[] mats = meshRenderer.materials;
         mats[2] = isOn ? screenVideo : screenOff;
         meshRenderer.materials = mats;
+
         if (videoPlayer != null)
         {
             if (isOn)
                 videoPlayer.Play();
             else
                 videoPlayer.Stop();
+        }
+
+        if (arm != null)
+        {
+            if (isOn)
+            {
+                arm.SetActive(true);
+                if (armAnimator != null && reachClip != null)
+                {
+                    armAnimator.Play(reachClip.name, -1, 0f);
+                }
+            }
+            else
+            {
+                arm.SetActive(false);
+            }
         }
     }
 }
