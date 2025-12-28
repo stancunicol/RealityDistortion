2afec5d718f8c2920c1644636ab8accc3d5e644b
Stancu Nicol-Alexia <nicol.stancu@student.unitbv.ro>
2025-12-28 10:40:11 +0200

    Added two zones that play footsteps sounds.

---
+++ RealityDistortion/Assets/Scripts/FootstepZoneDirect.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FootstepZoneDirect : MonoBehaviour
+{
+    public AudioClip leftClip;
+    public AudioClip rightClip;
+    public int repeatCount = 3;
+    public float interval = 0.3f;
+    public float triggerRadius = 1f;
+
+    private bool isPlaying = false;
+
+    void Update()
+    {
+        Collider[] hits = Physics.OverlapSphere(transform.position, triggerRadius);
+        foreach (var hit in hits)
+        {
+            if (hit.CompareTag("Player") && !isPlaying)
+            {
+                StartCoroutine(PlayFootsteps(hit.transform.position));
+            }
+        }
+    }
+
+    private IEnumerator PlayFootsteps(Vector3 position)
+    {
+        isPlaying = true;
+
+        for (int i = 0; i < repeatCount; i++)
+        {
+            AudioSource.PlayClipAtPoint(leftClip, position);
+            yield return new WaitForSeconds(interval);
+            AudioSource.PlayClipAtPoint(rightClip, position);
+            yield return new WaitForSeconds(interval);
+        }
+
+        isPlaying = false;
+    }
+}
