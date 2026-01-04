63f32ef0c3a672eeb2b23beb1b4d2476de296d6d
vasilemarina <marinavasile826@gmail.com>
2026-01-04 20:14:39 +0200

    Added doll anomaly: switches to creepy variant when not looked at.

---
+++ RealityDistortion/Assets/Scripts/Anomalies/Doll.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class Doll : MonoBehaviour
+{
+    [SerializeField] private Transform playerCamera;
+    [SerializeField] private GameObject normalDoll;
+    [SerializeField] private GameObject creepyDoll;
+    [SerializeField] private float viewAngle = 60f;
+
+    void Update()
+    {
+        Vector3 directionToDoll = (transform.position - playerCamera.position).normalized;
+        float angle = Vector3.Angle(playerCamera.forward, directionToDoll);
+
+        if (angle < viewAngle)
+        {
+            normalDoll.SetActive(false);
+            creepyDoll.SetActive(true);
+        }
+        else
+        {
+            normalDoll.SetActive(true);
+            creepyDoll.SetActive(false);
+        }
+    }
+}
