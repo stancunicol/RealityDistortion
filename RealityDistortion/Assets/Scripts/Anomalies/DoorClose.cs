efed6d700a9a5dd4ce163f0f99f5b7846f00857c
Stancu Nicol-Alexia <nicol.stancu@student.unitbv.ro>
2026-01-05 08:45:47 +0200

    Added a laying body in a room. If the player approaches, the door closes.

---
+++ RealityDistortion/Assets/Scripts/Anomalies/DoorClose.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+
+public class DoorClose : MonoBehaviour
+{
+    public Transform player;
+    public float closeDistance = 3f;
+    public Vector3 closedRotation;
+    private Vector3 openRotation;
+
+    private bool isClosed = false;
+
+    void Start()
+    {
+        openRotation = transform.eulerAngles;
+    }
+
+    void Update()
+    {
+        if (isClosed) return;
+
+        float dist = Vector3.Distance(player.position, transform.position);
+        if (dist <= closeDistance)
+        {
+            CloseDoor();
+        }
+    }
+
+    void CloseDoor()
+    {
+        transform.eulerAngles = closedRotation;
+        isClosed = true;
+    }
+}
